63f14d02d8577dd55ad57d90f8a8e04b10b80157
Felipe Montenegro Ribeiro <felipe.mribeiro@al.infnet.edu.br>
2021-07-10 16:12:44 -0300

    js v1

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 17,
+                nome: 'Jair Messias Bolsonaro',
+                partido: 'Sem partido',
+                fotos:[
+                    {url: 'bolso1.JPG', legenda: 'Presidente'}
+                ]
+            },
+            {
+                numero: 13,
+                nome: 'Luiz Inácio Lula da Silva',
+                partido: 'PT',
+                fotos: [
+                    {url: 'lula.JPG', legenda: 'Presidente'}
+                ]
+            },
+        ]
+    },
+    
+    {
+        titulo: 'PREFEITO',
+        numeros: 4,
+        candidatos: [
+            {
+                numero: 9812,
+                nome: 'Eduardo Paes',
+                partido: 'PSDB',
+                vice: 'joaozinho',
+                fotos: [
+                    {url: 'paes.JPG', legenda: 'Prefeito'},
+                    {url: 'vice-paes.JPG', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 3900,
+                nome: 'Juquinha Souza',
+                partido: 'Partido Verde',
+                vice: 'Zezinho',
+                fotos: [
+                    {url: 'mourao.JPG', legenda: 'Prefeito'},
+                    {url: 'mourao.JPG', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    },
+]
