4b0e784140668c7e91af93c6ec69cc67227f0ddb
Felipe Montenegro Ribeiro <felipe.mribeiro@al.infnet.edu.br>
2021-07-19 13:23:09 -0300

    script

--- script.js
+++ script.js
@@ -4,3 +4,49 @@ let descricao = document.querySelector('.d-1-4')
 let aviso = document.querySelector('.d-2')
 let lateral = document.querySelector('.d-1-right')
 let numeros = document.querySelector('.d-1-3')
+
+let etapaAtual = 0
+let numero = ''
+
+function startEtapa() {
+    let etapa = etapas[etapaAtual]  //Funcao onde a variável etapa recebe o array dos candidatos a presidencia
+
+    let numeroHtml = ''
+
+    for(let i=0; i<etapa.numeros; i++) {    // laço for para incrementar o numero de deigitos para o tipo de candidato
+        if(i === 0){
+            numeroHtml += '<div class="numero pisca"></div>'
+        } else {
+            numeroHtml += '<div class="numero"></div>'
+        }
+    }
+
+    seuVotoPara.style.display = 'none'   //limpando os espaços
+    cargo.innerHTML = etapa.titulo
+    descricao.innerHTML = ''
+    aviso.style.display = 'none'
+    lateral.innerHTML = ''
+    numeros.innerHTML = numeroHtml
+}
+
+function atualizaInterface() {
+
+}
+
+function clicou(n) {
+    let elNumero = document.querySelector('.numero pisca')
+    if(elNumero !== null)
+        elNumero.innerHTML = n
+        numero = `${numero}${n}`
+}
+function branco() {
+    alert('clicou em branco')
+}
+function corrige() {
+    alert('clicou em corrige')
+}
+function confirma() {
+    alert('clicou em confirma')
+}
+
+startEtapa()
